d68a386985d313808337f29c8eac995e2947d2fa
DimaM190 <dima.mahuga@gmail.com>
2026-02-14 17:36:48 +0200

    strung

---
+++ lesson3_strings.py
@@ -0,0 +1,49 @@
+some_string = (
+    " m\n55  my name is Dima namer dddddd    SSSSSSSSs 4445656565656565       "
+)
+
+# upper = some_string.upper()  # 'MYNAME IS DIMA  DDDDDD    SSSSSSSSS'
+# lower = some_string.lower()  # 'myname is dima  dddddd    sssssssss'
+# title = some_string.title()  # 'Myname Is Dima  Dddddd    Sssssssss'
+# capitalize = some_string.capitalize()  # 'Myname is dima  dddddd    sssssssss'
+#
+# chain = some_string.lower().capitalize()
+#
+# clear_string_spaces = some_string.strip()
+# clear_string_symbols = some_string.strip(" 56naMe")
+# clear_string_symbols_left = some_string.lstrip(" 56naMe")
+# clear_string_symbols_right = some_string.rstrip(" 56naMe")
+#
+# change_inner_text = (
+#     some_string.replace("name", "surename", 1)
+#     .replace("Dima", "Igor")
+#     .replace("namer", "")
+# )
+# table = str.maketrans("56", "65", "\n")
+# upper = some_string.upper()  # 'MYNAME IS DIMA  DDDDDD    SSSSSSSSS'
+# lower = some_string.lower()  # 'myname is dima  dddddd    sssssssss'
+# title = some_string.title()  # 'Myname Is Dima  Dddddd    Sssssssss'
+# capitalize = some_string.capitalize()  # 'Myname is dima  dddddd    sssssssss'
+#
+# chain = some_string.lower().capitalize()
+#
+# clear_string_spaces = some_string.strip()
+# clear_string_symbols = some_string.strip(" 56naMe")
+# clear_string_symbols_left = some_string.lstrip(" 56naMe")
+# clear_string_symbols_right = some_string.rstrip(" 56naMe")
+#
+# change_inner_text = (
+#     some_string.replace("name", "surename", 1)
+#     .replace("Dima", "Igor")
+#     .replace("namer", "")
+# )
+# table = str.maketrans("56", "65", "\n")
+# result_smsrt_change = some_string.translate(table)
+
+slices1 = some_string[:]
+slices2 = some_string[12:20]
+slices3 = some_string[12:20:2]
+slices4 = some_string[-3:-50:-1]
+
+
+pass
