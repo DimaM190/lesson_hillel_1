a0c21598288a4041e725f1d53d270ebfc718949c
DimaM190 <dima.mahuga@gmail.com>
2026-02-15 12:58:22 +0200

    code stayl blasck

---
+++ numbers_lesson.py
@@ -0,0 +1,38 @@
+some_number = "   4564    "
+some_number = some_number.strip()
+
+
+some_number = int(some_number)
+
+some_number_mult_2 = some_number * 2
+
+namber_value = 4_546_454
+
+nul_mult = namber_value * 5
+nul_mult *= 2  # сами себя умножили на 2  nul_mult = nul_mult*2
+
+nul_mult /= 4  # nul_mult = nul_mult / 4
+
+num1 = 0.1
+num2 = 0.2
+num3 = num1 + num2
+num4 = num1 - num3
+
+negative_number1 = -1
+negative_number2 = -156.126
+negative_negative_number2 = round(negative_number2, 2)
+negative_bad_number2 = -156.0  # всегда ставить 0 после точки
+
+power = 5**2
+power1 = 5**3.5
+root = 25**0.5
+
+div1 = 26 // 5
+rest = 26 % 5
+hw = 123235
+hw_1 = hw // 100000
+hw_2 = (hw - hw_1 * 100000) // 10000
+revers = hw_2 * 100000 + hw_1 * 10000
+
+
+pass
