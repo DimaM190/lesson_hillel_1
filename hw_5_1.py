5afec057391a06162bb8019654182543ec323cdf
DimaM190 <dima.mahuga@gmail.com>
2026-03-03 21:00:02 +0200

    !!!

--- hw_5_1.py
+++ hw_5_1.py
@@ -1,5 +1,29 @@
-inp_name_var = input("Enter name variable for checking: ")
+import string
+import keyword
 
-if
+name_var = input("Enter name variable for checking: ")
 
-pass
+if name_var == "":
+    print(False)
+    exit()
+if name_var[0].isdigit():
+    print(False)
+    exit()
+if name_var in keyword.kwlist:
+    print(False)
+    exit()
+if name_var.count("_") > 1:
+    print(False)
+    exit()
+
+for elm in name_var:
+    if elm.isupper():
+        print(False)
+        exit()
+    if elm in string.punctuation and elm != "_":
+        print(False)
+        exit()
+    if elm == " ":
+        print(False)
+        exit()
+print(True)
