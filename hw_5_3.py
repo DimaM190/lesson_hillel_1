d7b6aaaaa57e8cf1c9a6b7b70fcefe40aa556867
DimaM190 <dima.mahuga@gmail.com>
2026-03-12 19:26:26 +0200

    done

--- hw_5_3.py
+++ hw_5_3.py
@@ -1,1 +1,14 @@
+import string
+
 has_htag = input("Enter expression: ")
+
+has_htag1 = has_htag.title()
+
+del_pun = ""
+for elm in has_htag1:
+    if elm not in string.punctuation and elm != " ":
+        del_pun = del_pun + elm
+
+
+print("#" + del_pun[0:140])
+pass
